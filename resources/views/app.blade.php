895f04e8d40e0e8d03714d7fa7dc645df88fab5f
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-01-10 15:17:20 +0100

    #9 - fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet"/>
+    <script src="{{ asset('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
 <body>
