029fc0b413cbdc78ff520e2c89e69ae22d9274e2
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-01-11 13:04:11 +0100

    Update resources/js/Pages/Dashboard.vue
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -628,7 +628,7 @@ const recentHires = [
             'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         href: '#',
     },
-]
+];
 const announcements = [
     {
         id: 1,
