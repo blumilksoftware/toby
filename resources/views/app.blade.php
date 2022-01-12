e015f02892508b9d7fb4669204ddf41a06014469
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-01-12 14:18:25 +0100

    #11 - basic layout (#15)
    * #11 - basic layout
    
    * #5 - bump dependencies
    
    * #11 - fix
    
    * #11 - fix
    
    * #11 - fix logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <link rel="icon" href="/img/icon.png">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ asset('/js/app.js') }}" defer></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'blumilk': {
+                    '50': '#D5DFEE',
+                    '100': '#C7D4E9',
+                    '200': '#AABDDD',
+                    '300': '#8CA7D1',
+                    '400': '#6F90C6',
+                    '500': '#527ABA',
+                    '600': '#3C5F97',
+                    '700': '#2C466F',
+                    '800': '#1C2D47',
+                    '900': '#0C141F'
+                },
+            }
         },
     },
     plugins: [
