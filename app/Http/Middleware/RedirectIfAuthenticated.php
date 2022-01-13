63f5a28794338ea4ca4d0271218a88d0cf6adaa4
Adrian Hopek <adrian.hopek@gmail.com>
2022-01-13 13:25:39 +0100

    #2 - fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect()->route("login");
+                return redirect()->route("dashboard");
             }
         }
 
