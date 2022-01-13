1f11c7175451856fb2482fc294622a5d8a9f2559
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-01-13 13:50:13 +0100

    Apply suggestions from code review
    Co-authored-by: Krzysztof Rewak <krzysztof.rewak@blumilk.pl>

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -12,7 +12,7 @@ class LogoutController extends Controller
 {
     public function __invoke(Request $request): RedirectResponse
     {
-        Auth::logout();
+        $request->user()?->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
