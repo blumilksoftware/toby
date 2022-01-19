2dff7995b1cf612a75435930069e91ed2a09d261
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-01-19 10:47:02 +0100

    #19 - fix

--- app/Jobs/CheckYearPeriod.php
+++ app/Jobs/CheckYearPeriod.php
@@ -7,17 +7,13 @@ namespace Toby\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
 use Toby\Models\YearPeriod;
 
 class CheckYearPeriod implements ShouldQueue
 {
     use Dispatchable;
-    use InteractsWithQueue;
     use Queueable;
-    use SerializesModels;
 
     public function handle(): void
     {
