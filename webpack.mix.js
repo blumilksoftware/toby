529b9d084aaef3faf2fdf953f6d192e01d81b2db
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-02-15 09:02:58 +0100

    #22 - fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
-const mix = require('laravel-mix');
-const webpackConfig = require('./webpack.config');
+const mix = require('laravel-mix')
+const webpackConfig = require('./webpack.config')
 
-mix.js("resources/js/app.js", "public/js")
-    .vue(3)
-    .postCss("resources/css/app.css", "public/css", [
-        require("tailwindcss"),
-    ])
-    .webpackConfig(webpackConfig)
-    .sourceMaps();
+mix.js('resources/js/app.js', 'public/js')
+  .vue(3)
+  .postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+  ])
+  .webpackConfig(webpackConfig)
+  .sourceMaps()
