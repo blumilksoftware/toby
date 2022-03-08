099fa2677f7961a57d7f56c1c011a55242147d1e
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-03-08 10:54:24 +0100

    test

--- config/database.php
+++ config/database.php
@@ -30,6 +30,7 @@ return [
             "prefix" => env("REDIS_PREFIX", Str::slug(env("APP_NAME", "laravel"), "_") . "_database_"),
         ],
         "default" => [
+            "scheme" => "tls",
             "url" => env("REDIS_URL"),
             "host" => env("REDIS_HOST", "127.0.0.1"),
             "password" => env("REDIS_PASSWORD"),
