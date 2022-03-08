4f17a7021c17721e577197223ce9939f52c24662
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-03-08 14:55:44 +0100

    test

--- app/Eloquent/Observers/UserObserver.php
+++ app/Eloquent/Observers/UserObserver.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace Toby\Eloquent\Observers;
 
-use Toby\Eloquent\Helpers\YearPeriodRetriever;
 use Toby\Eloquent\Models\User;
 use Toby\Eloquent\Models\YearPeriod;
 
 class UserObserver
 {
-    public function __construct(
-        protected YearPeriodRetriever $yearPeriodRetriever,
-    ) {}
-
     public function created(User $user): void
     {
         $yearPeriods = YearPeriod::all();
