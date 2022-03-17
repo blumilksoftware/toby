88d3ac028dc50f7dd6ebc73a593631d7bdf283cc
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-03-17 12:59:42 +0100

    #85 - google calendar improvements

--- database/factories/VacationRequestFactory.php
+++ database/factories/VacationRequestFactory.php
@@ -15,7 +15,6 @@ use Toby\Eloquent\Models\YearPeriod;
 class VacationRequestFactory extends Factory
 {
     protected $model = VacationRequest::class;
-    private static int $number = 1;
 
     public function definition(): array
     {
@@ -26,20 +25,11 @@ class VacationRequestFactory extends Factory
             "user_id" => User::factory(),
             "creator_id" => fn(array $attributes): int => $attributes["user_id"],
             "year_period_id" => YearPeriod::factory(),
-            "name" => fn(array $attributes): string => $this->generateName($attributes),
             "type" => $this->faker->randomElement(VacationType::cases()),
             "state" => $this->faker->randomElement(VacationRequestStatesRetriever::all()),
             "from" => $from,
             "to" => $from->addDays($days),
             "comment" => $this->faker->boolean ? $this->faker->paragraph() : null,
         ];
-    }
-
-    protected function generateName(array $attributes): string
-    {
-        $year = YearPeriod::find($attributes["year_period_id"])->year;
-        $number = static::$number++;
-
-        return "{$number}/{$year}";
     }
 }
---
+++ database/migrations/2022_03_17_105927_add_event_ids_column_and_remove_name_column_in_vacation_requests_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    public function up(): void
+    {
+        Schema::table("vacation_requests", function (Blueprint $table): void {
+            $table->json("event_ids")->nullable();
+            $table->dropColumn("name");
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table("vacation_requests", function (Blueprint $table): void {
+            $table->dropColumn("event_ids");
+            $table->string("name");
+        });
+    }
+};
