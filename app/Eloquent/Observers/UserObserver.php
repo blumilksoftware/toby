7473f7aaf1a2dccedb67db41d74323f9127a736f
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-03-21 14:33:45 +0100

    #84 - cr fix

--- app/Eloquent/Observers/UserObserver.php
+++ app/Eloquent/Observers/UserObserver.php
@@ -4,20 +4,24 @@ declare(strict_types=1);
 
 namespace Toby\Eloquent\Observers;
 
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Support\Str;
 use Toby\Eloquent\Models\User;
 use Toby\Eloquent\Models\YearPeriod;
 
 class UserObserver
 {
+    public function __construct(
+        protected Hasher $hash,
+    ) {}
+
     public function creating(User $user): void
     {
         /**
          * A random password for user is generated because AuthenticateSession middleware needs a user's password
          * for some checks. Users use Google to login, so they don't need to know the password (GitHub issue #84)
          */
-        $user->password = Hash::make(Str::random(40));
+        $user->password = $this->hash->make(Str::random(40));
     }
 
     public function created(User $user): void
