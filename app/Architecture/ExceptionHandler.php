9dba729b58e7d5bd717df22a4a03be52625c5864
EwelinaLasowy <ewelina.lasowy@blumilk.pl>
2022-03-28 08:53:07 +0200

    #93 - wip

--- app/Architecture/ExceptionHandler.php
+++ app/Architecture/ExceptionHandler.php
@@ -21,18 +21,12 @@ class ExceptionHandler extends Handler
     {
         $response = parent::render($request, $e);
 
-        if (!app()->environment(["local", "testing"]) && in_array($response->status(), [500, 503, 404, 403], true)) {
+        if (!app()->environment(["local", "testing"]) && in_array($response->status(), [500, 503, 419, 404, 403], true)) {
             return Inertia::render("Error", [
                 "status" => $response->status(),
             ])
                 ->toResponse($request)
                 ->setStatusCode($response->status());
-        }
-
-        if ($response->status() === 419) {
-            return back()->with([
-                "message" => "The page expired, please try again.",
-            ]);
         }
 
         return $response;
