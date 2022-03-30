ab16af1ca93c543c88ec11a40747c365205b9787
Ewelina Lasowy <56546832+EwelinaLasowy@users.noreply.github.com>
2022-03-30 09:54:29 +0200

    - Permissions tests (#97)
    * - added some test
    
    * - cr fix
    
    * - cr fix
    
    * - cr fix
    
    * - cr fix

---
+++ tests/Feature/VacationCalendarTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\FeatureTestCase;
+use Toby\Eloquent\Models\User;
+
+class VacationCalendarTest extends FeatureTestCase
+{
+    use DatabaseMigrations;
+
+    public function testAdministrativeApproverCanDownloadTimesheet(): void
+    {
+        $administrativeApprover = User::factory()->administrativeApprover()->createQuietly();
+
+        $this->actingAs($administrativeApprover)
+            ->get("/timesheet/january")
+            ->assertOk();
+    }
+
+    public function testEmployeeCannotDownloadTimesheet(): void
+    {
+        $user = User::factory()->createQuietly();
+
+        $this->actingAs($user)
+            ->get("/timesheet/january")
+            ->assertForbidden();
+    }
+}
