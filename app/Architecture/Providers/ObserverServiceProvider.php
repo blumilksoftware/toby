edbe5bb9612c0f5a643f53923b6c73e237920d54
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-04-04 14:23:51 +0200

    wip

--- app/Architecture/Providers/ObserverServiceProvider.php
+++ app/Architecture/Providers/ObserverServiceProvider.php
@@ -6,12 +6,15 @@ namespace Toby\Architecture\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Toby\Eloquent\Models\User;
+use Toby\Eloquent\Models\VacationRequest;
 use Toby\Eloquent\Observers\UserObserver;
+use Toby\Eloquent\Observers\VacationRequestObserver;
 
 class ObserverServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         User::observe(UserObserver::class);
+        VacationRequest::observe(VacationRequestObserver::class);
     }
 }
