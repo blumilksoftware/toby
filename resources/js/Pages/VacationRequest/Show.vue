62588e0dbe614f60e24cf36ac8e098c9b1d566eb
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-04-06 14:57:43 +0200

    #108 - fix

--- resources/js/Pages/VacationRequest/Show.vue
+++ resources/js/Pages/VacationRequest/Show.vue
@@ -41,7 +41,7 @@
             </div>
             <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">
-                Rodzaj urlopu
+                Rodzaj wniosku
               </dt>
               <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                 <VacationType :type="request.type" />
