9c9b36cb1b9acb089439f93cf51b5773a243282a
Adrian Hopek <adrian.hopek@blumilk.pl>
2022-04-07 14:11:14 +0200

    #112 - change content of emails

--- app/Domain/Notifications/VacationRequestWaitsForApprovalNotification.php
+++ app/Domain/Notifications/VacationRequestWaitsForApprovalNotification.php
@@ -8,6 +8,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use InvalidArgumentException;
+use Toby\Domain\States\VacationRequest\WaitingForTechnical;
 use Toby\Eloquent\Models\User;
 use Toby\Eloquent\Models\VacationRequest;
 
@@ -43,8 +44,6 @@ class VacationRequestWaitsForApprovalNotification extends Notification
     protected function buildMailMessage(string $url): MailMessage
     {
         $user = $this->user->first_name;
-        $requester = $this->vacationRequest->user->fullName;
-        $title = $this->vacationRequest->name;
         $type = $this->vacationRequest->type->label();
         $from = $this->vacationRequest->from->toDisplayString();
         $to = $this->vacationRequest->to->toDisplayString();
@@ -54,13 +53,8 @@ class VacationRequestWaitsForApprovalNotification extends Notification
             ->greeting(__("Hi :user!", [
                 "user" => $user,
             ]))
-            ->subject(__("Vacation request :title is waiting for your approval", [
-                "title" => $title,
-            ]))
-            ->line(__("The vacation request :title from user: :requester is waiting for your approval.", [
-                "title" => $title,
-                "requester" => $requester,
-            ]))
+            ->subject($this->buildSubject())
+            ->line($this->buildDescription())
             ->line(__("Vacation type: :type", [
                 "type" => $type,
             ]))
@@ -71,4 +65,38 @@ class VacationRequestWaitsForApprovalNotification extends Notification
             ]))
             ->action(__("Click here for details"), $url);
     }
+
+    protected function buildSubject(): string
+    {
+        $title = $this->vacationRequest->name;
+
+        if ($this->vacationRequest->state->equals(WaitingForTechnical::class)) {
+            return __("Vacation request :title is waiting for your technical approval", [
+                "title" => $title,
+            ]);
+        }
+
+        return __("Vacation request :title is waiting for your administrative approval", [
+            "title" => $title,
+        ]);
+    }
+
+    protected function buildDescription(): string
+    {
+        $title = $this->vacationRequest->name;
+        $requester = $this->vacationRequest->user->fullName;
+
+        if ($this->vacationRequest->state->equals(WaitingForTechnical::class)) {
+            return __("The vacation request :title from user :requester is waiting for your technical approval.", [
+                "title" => $title,
+                "requester" => $requester,
+            ]);
+        }
+
+        return __("The vacation request :title from user :requester is waiting for your administrative approval.", [
+            "title" => $title,
+            "requester" => $requester,
+        ]);
+    }
 }
+
