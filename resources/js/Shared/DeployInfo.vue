c16943b623ea0ba09a0c0657914fe6935f36fd4e
Kamil Niemczycki <contact@kamilcraft.com>
2022-07-05 12:32:07 +0200

    #172 - content improvement

--- resources/js/Shared/DeployInfo.vue
+++ resources/js/Shared/DeployInfo.vue
@@ -4,19 +4,8 @@
     class="min-w-full p-4 text-xs text-gray-500 flex gap-x-1"
   >
     <p class="flex flex-row gap-x-1">
-      <strong>Wydanie:</strong>
-      <a
-        v-if="deployInformation.github_url"
-        class="text-blumilk-600 hover:underline hover:text-blumilk-500"
-        :href="`${deployInformation.github_url}/commit/${deployInformation.slug_commit}`"
-        target="_blank"
-        rel="noopener nofollow noreferrer"
-        :title="`Commit: ${deployInformation.slug_commit ?? 'unset'}`"
-      >
-        {{ deployInformation.slug_description ?? 'unset' }} ({{ deployInformation.release_version }})
-      </a>
+      <strong>Wersja:</strong>
       <span
-        v-else
         :title="`Commit: ${deployInformation.slug_commit ?? 'unset'}`"
       >
         {{ deployInformation.slug_description ?? 'unset' }} ({{ deployInformation.release_version ?? 'unset' }})
@@ -27,8 +16,8 @@
       class="flex flex-row gap-x-1"
       :class="{ 'border-l-2 border-gray-300 pl-1': isHorizontal() }"
     >
-      <strong>Data wydania:</strong>
-      <span>{{ DateTime.fromISO(deployInformation.release_created_at).toLocaleString(DateTime.DATETIME_SHORT) }}</span>
+      <strong>Ostatnio zbudowano:</strong>
+      <span>{{ getDate() }}</span>
     </p>
     <p
       v-if="deployInformation.github_url"
@@ -37,7 +26,7 @@
       <strong v-if="isVertical()">Repozytorium:</strong>
       <a
         v-if="deployInformation.github_url"
-        class="text-blumilk-600 hover:underline hover:text-blumilk-500"
+        class="text-gray-800 underline hover:no-underline hover:text-blumilk-600"
         :href="deployInformation.github_url"
         target="_blank"
         rel="noopener nofollow noreferrer"
@@ -63,5 +52,16 @@ function isVertical() {
 
 function isHorizontal() {
   return props.layout === 'horizontal'
+}
+
+function getDate() {
+  return DateTime.fromISO(props.deployInformation.release_created_at)
+    .toLocaleString({
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    })
 }
 </script>
