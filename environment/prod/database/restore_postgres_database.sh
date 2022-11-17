0a4aee162244f73313dd7cd0a5a3ef3f3d744c05
Marcin Tracz <marcin.tracz@blumilk.pl>
2022-11-17 14:09:10 +0100

    #218 - switch from docker-compose to docker in restore postgres db script

--- environment/prod/database/restore_postgres_database.sh
+++ environment/prod/database/restore_postgres_database.sh
@@ -1,18 +1,18 @@
 # example:
-# bash restore_postgres_database.sh <filename> <database name> <username> <docker service name>
+# bash restore_postgres_database.sh <filename> <database name> <username> <docker container name>
 
 DUMP_FILE_NAME=${1?provide dump file name as 1st argument}
 DATABASE_NAME=${2?provide database name as 2nd argument}
 DATABASE_USERNAME=${3?provide database usernname as 3rd argument}
-DOCKER_COMPOSE_POSTGRES_SERVICE=${4?provide postgres docker compose service as 4th argument}
-DOCKER_COMPOSE_FILE_PATH=./../../../docker-compose.beta.yml
+DOCKER_POSTGRES_CONTAINER_NAME=${4?provide postgres docker container name as 4th argument}
+DESTINATION_DIR=/var/lib/postgresql
 
-echo "Copying dump file (${DUMP_FILE_NAME}) into container (${DOCKER_COMPOSE_POSTGRES_SERVICE})" \
-  && docker cp ${DUMP_FILE_NAME} ${DOCKER_COMPOSE_POSTGRES_SERVICE}:/var/lib/postgresql/${DUMP_FILE_NAME} \
+echo "Copying dump file (${DUMP_FILE_NAME}) into container (${DOCKER_POSTGRES_CONTAINER_NAME})" \
+  && docker cp ${DUMP_FILE_NAME} ${DOCKER_POSTGRES_CONTAINER_NAME}:${DESTINATION_DIR}/${DUMP_FILE_NAME} \
   && echo "Done"
 
-echo "restoring data from file: (/var/lib/postgresql/${DUMP_FILE_NAME}) into database: ${DATABASE_NAME}" \
-  && docker-compose -f $DOCKER_COMPOSE_FILE_PATH exec -T --workdir /var/lib/postgresql ${DOCKER_COMPOSE_POSTGRES_SERVICE} \
+echo "restoring data from file: (${DESTINATION_DIR}/${DUMP_FILE_NAME}) into database: ${DATABASE_NAME}" \
+  && docker exec --workdir ${DESTINATION_DIR} ${DOCKER_POSTGRES_CONTAINER_NAME} \
       pg_restore \
       --exit-on-error \
       --verbose \
@@ -25,5 +25,5 @@ echo "restoring data from file: (/var/lib/postgresql/${DUMP_FILE_NAME}) into dat
       ${DUMP_FILE_NAME} \
   && echo "Done." \
   && echo "Removing dump file from container" \
-  && docker-compose -f $DOCKER_COMPOSE_FILE_PATH exec -T --workdir /var/lib/postgresql ${DOCKER_COMPOSE_POSTGRES_SERVICE} rm ${DUMP_FILE_NAME} \
+  && docker exec --workdir ${DESTINATION_DIR} ${DOCKER_POSTGRES_CONTAINER_NAME} rm ${DUMP_FILE_NAME} \
   && echo "Done."
