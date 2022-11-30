1d2d9f635bb624a29426fccc14d3d1d15ba283d7
Marcin Tracz <marcin.tracz@blumilk.pl>
2022-11-30 14:05:54 +0100

    #252 - test cron

--- app/Infrastructure/Console/Kernel.php
+++ app/Infrastructure/Console/Kernel.php
@@ -38,17 +38,18 @@ class Kernel extends ConsoleKernel
     protected function scheduleDatabaseBackup(Schedule $schedule): void
     {
         $scheduledTask = $schedule->command(BackupPostgresDatabase::class)
-            ->dailyAt(time: "05:00")
-            ->withoutOverlapping()
-            ->onOneServer()
-            ->environments(["beta", "production"]);
-
-        $notifyOnFailure = config()->get("mail.database_backup.notify_on_failure");
-
-        if ($notifyOnFailure) {
-            $emailAddress = config()->get("mail.database_backup.notification_email");
-
-            $scheduledTask->emailOutputOnFailure($emailAddress);
-        }
+            ->everyMinute();
+//            ->dailyAt(time: "05:00")
+//            ->withoutOverlapping()
+//            ->onOneServer()
+//            ->environments(["beta", "production"]);
+//
+//        $notifyOnFailure = config()->get("mail.database_backup.notify_on_failure");
+//
+//        if ($notifyOnFailure) {
+//            $emailAddress = config()->get("mail.database_backup.notification_email");
+//
+//            $scheduledTask->emailOutputOnFailure($emailAddress);
+//        }
     }
 }
