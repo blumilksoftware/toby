a14e72781062bcdea9ea589c7470a841234e71d8
Marcin Tracz <marcin.tracz@blumilk.pl>
2022-11-30 10:25:07 +0100

    Revert "#252 - fix cron"
    This reverts commit 0f3b1e46a236fca44c4835beda9aee5ab071b8e3.

--- environment/prod/scheduler/entrypoint.sh
+++ environment/prod/scheduler/entrypoint.sh
@@ -3,8 +3,7 @@
 # -e is for "automatic error detection", tell shell to abort any time an error occurred
 set -e
 
-# https://manpages.debian.org/bullseye/cron/cron.8.en.html
-# -f  Stay in foreground mode, don't daemonize.
+# -f  foreground
+# -L log output
 
-crontab ./environment/prod/scheduler/tasks.cron \
-  && cron -f
+crontab ./environment/prod/scheduler/tasks.cron && crond -f -L /dev/stdout
