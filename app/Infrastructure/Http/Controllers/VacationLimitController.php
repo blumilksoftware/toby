41e7816143d4b7c6ffcc6732d7d4c7d464dd0363
EwelinaLasowy <ewelina.lasowy@blumilk.pl>
2022-12-02 11:21:09 +0100

    #268 - fix

--- app/Infrastructure/Http/Controllers/VacationLimitController.php
+++ app/Infrastructure/Http/Controllers/VacationLimitController.php
@@ -92,6 +92,6 @@ class VacationLimitController extends Controller
 
         return redirect()
             ->back()
-            ->with("success", __(":)"));
+            ->with("success", __("Days moved."));
     }
 }
