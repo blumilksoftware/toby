f6981bb6702802b1b4b80bd18f7113e8ca171505
Kamil <kamil.piech.97@gmail.com>
2024-03-20 14:38:52 +0100

    - laod fix

--- app/Infrastructure/Http/Middleware/HandleInertiaRequests.php
+++ app/Infrastructure/Http/Middleware/HandleInertiaRequests.php
@@ -32,8 +32,7 @@ class HandleInertiaRequests extends Middleware
 
     protected function getAuthData(Request $request): Closure
     {
-        $user = $request->user();
-        $user->load("profile");
+        $user = $request->user()?->load("profile");
 
         return fn(): array => [
             "user" => $user ? new UserResource($user) : null,
