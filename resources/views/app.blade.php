9d351055f649109addec5e86729471a416082f3f
Kamil <kamil.piech.97@gmail.com>
2024-03-26 12:48:19 +0100

    - updated csp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; frame-src 'self'; object-src 'none'; require-trusted-types-for 'script';">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' https://rsms.me/inter/inter.css 'unsafe-inline'; img-src 'self' https://eu.ui-avatars.com/ data:; font-src 'self' https://rsms.me/ ; base-uri 'self'; form-action 'self'; frame-src 'self'; object-src 'none';">
     <link rel="icon" href="/images/icon.png">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite('resources/js/app.js')
