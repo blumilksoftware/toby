18ce4be52062f448e4ba00622bcda0b1a1551024
Kamil Piech <kamil.piech@blumilk.pl>
2024-07-23 10:08:05 +0200

    #452 - bug fix (#472)
    * #452 - fix: fixed edit form
    
    * #452 - fix: create view fix

--- app/Http/Resources/UserFormDataResource.php
+++ app/Http/Resources/UserFormDataResource.php
@@ -20,13 +20,8 @@ class UserFormDataResource extends JsonResource
             "role" => $this->role,
             "position" => $this->profile->position,
             "employmentForm" => $this->profile->employment_form,
-            "employmentDate" => $this->profile->employment_date->toDateString(),
             "birthday" => $this->profile->birthday->toDateString(),
             "slackId" => $this->profile->slack_id,
-            "lastMedicalExamDate" => $this->profile->last_medical_exam_date?->toDateString(),
-            "nextMedicalExamDate" => $this->profile->next_medical_exam_date?->toDateString(),
-            "lastOhsTrainingDate" => $this->profile->last_ohs_training_date?->toDateString(),
-            "nextOhsTrainingDate" => $this->profile->next_ohs_training_date?->toDateString(),
         ];
     }
 }
