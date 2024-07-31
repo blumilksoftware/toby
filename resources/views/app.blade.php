874b110908eead375e39d26cc82d05935e854fab
Kamil Piech <kamil.piech@blumilk.pl>
2024-07-31 09:10:07 +0200

    #395 - CSP header (#412)
    * #395 - added csp header
    
    * #395 - fix
    
    * - updated csp
    
    * #395 - fix
    
    * #395 - fix 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    @if (app()->environment('production'))
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' https://rsms.me/inter/inter.css 'unsafe-inline'; img-src 'self' https://eu.ui-avatars.com/ data:; font-src 'self' https://rsms.me/ ; base-uri 'self'; form-action 'self'; frame-src 'self'; object-src 'none';">
+    @endif
     <link rel="icon" href="/images/icon.png">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite('resources/js/app.js')
