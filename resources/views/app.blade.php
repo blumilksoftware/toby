73b6266575cc4a96423af44cb07232e5982a679c
Kamil Piech <kamil.piech@blumilk.pl>
2024-07-31 11:37:29 +0200

    #395 - updated if statement for csp header (#477)
    #395 - fix: updated if statement for csp header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-    @if (app()->environment('production'))
+    @if (!app()->environment('local'))
         <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' https://rsms.me/inter/inter.css 'unsafe-inline'; img-src 'self' https://eu.ui-avatars.com/ data:; font-src 'self' https://rsms.me/ ; base-uri 'self'; form-action 'self'; frame-src 'self'; object-src 'none';">
     @endif
     <link rel="icon" href="/images/icon.png">
